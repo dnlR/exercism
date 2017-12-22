c3dc03f9d7ea5693821f87d87dbc13979a5aa2bc
Daniel Romero <hi@danielromero.com>
2017-12-22 10:33:50 +0100

    Finish helloWorld Elm

--- elm/hello-world/tests/Tests.elm
+++ elm/hello-world/tests/Tests.elm
@@ -15,12 +15,10 @@ tests =
         -- Once you get the first test passing, remove the
         -- `skip <|` (just leave the comma) on the next two
         -- lines to continue!
-        , skip <|
-            test "Hello to a sample name" <|
+        , test "Hello to a sample name" <|
                 \() ->
                     Expect.equal "Hello, Alice!" (helloWorld (Just "Alice"))
-        , skip <|
-            test "Hello to another sample name" <|
+        , test "Hello to another sample name" <|
                 \() ->
                     Expect.equal "Hello, Bob!" (helloWorld (Just "Bob"))
         ]
