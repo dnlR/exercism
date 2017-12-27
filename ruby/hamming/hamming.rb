dda249f752d0e35611cbef96f377c535ecbd59bd
Daniel Romero <hi@danielromero.com>
2017-12-27 19:00:59 +0100

    Improve hamming ruby

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,17 +1,6 @@
 class Hamming
   def self.compute(first_dna_strand, second_dna_strand)
-    if first_dna_strand.length != second_dna_strand.length then
-      raise ArgumentError.new("DNA strands must have the same length")
-    end
-
-    difference_count = 0
-
-    first_dna_strand.each_char.with_index(0) do |c, i|
-      if second_dna_strand[i] != c then
-        difference_count += 1
-      end
-    end
-
-    difference_count
+    raise ArgumentError unless first_dna_strand.length == second_dna_strand.length
+    first_dna_strand.chars.zip(second_dna_strand.chars).count { |l, r| l != r }
   end
 end
