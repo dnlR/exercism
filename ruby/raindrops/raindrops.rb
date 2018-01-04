7c49ecae082c910de734af196eecf524c1ef22ed
Daniel Romero <hi@danielromero.com>
2018-01-04 10:08:46 +0100

    Simplify raindrops ruby

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,11 +2,9 @@ class Raindrops
   def self.convert(number)
     raindrop_sound = ''
 
-    (1..number).each do |factor|
-      raindrop_sound += 'Pling' if number % factor == 0 and factor == 3
-      raindrop_sound += 'Plang' if number % factor == 0 and factor == 5
-      raindrop_sound += 'Plong' if number % factor == 0 and factor == 7
-    end
+    raindrop_sound << 'Pling' if number % 3 == 0
+    raindrop_sound << 'Plang' if number % 5 == 0
+    raindrop_sound << 'Plong' if number % 7 == 0
 
     raindrop_sound.empty? ? number.to_s : raindrop_sound
   end
