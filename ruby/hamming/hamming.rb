480f9c69a50ab7caf7af17fb6eb5dcb28c769a74
Daniel Romero <hi@danielromero.com>
2018-06-16 10:52:47 +0200

    updated

--- ruby/hamming/hamming.rb
+++
@@ -1,6 +0,0 @@
-class Hamming
-  def self.compute(first_dna_strand, second_dna_strand)
-    raise ArgumentError unless first_dna_strand.length == second_dna_strand.length
-    first_dna_strand.chars.zip(second_dna_strand.chars).count { |l, r| l != r }
-  end
-end
