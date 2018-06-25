e99cb5c4020fe2a4abbead4cd11d7a8ea35a787c
Daniel Romero <hi@danielromero.com>
2018-06-25 21:19:52 +0200

    py hamming

---
+++ python/hamming/hamming.py
@@ -0,0 +1,10 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("DNA strands with different length")
+
+    count = 0
+    for nucleotide_a, nucleotide_b in zip(strand_a, strand_b):
+        if nucleotide_a != nucleotide_b:
+            count += 1
+
+    return count
