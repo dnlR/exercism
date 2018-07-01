4810032b3ae50f46ba9311b6c7fa1f8491dce0ab
Daniel Romero <hi@danielromero.com>
2018-07-01 10:26:36 +0200

    refactor py yacht

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,45 +1,40 @@
 # Score categories
 # Change the values as you see fit
-ONES = 1
-TWOS = 2
-THREES = 3
-FOURS = 4
-FIVES = 5
-SIXES = 6
-FULL_HOUSE = 7
-FOUR_OF_A_KIND = 8
-LITTLE_STRAIGHT = 9
-BIG_STRAIGHT = 10
-CHOICE = 11
-YACHT = 12
+from collections import Counter
+
+ONES = "ONES"
+TWOS = "TWOS"
+THREES = "THREES"
+FOURS = "FOURS"
+FIVES = "FIVES"
+SIXES = "SIXES"
+FULL_HOUSE = "FULL HOUSE"
+FOUR_OF_A_KIND = "FOUR OF A KIND"
+LITTLE_STRAIGHT = "LITTLE STRAIGHT"
+BIG_STRAIGHT = "BIG STRAIGHT"
+CHOICE = "CHOICE"
+YACHT = "YACHT"
 
 
 def score(dice, category):
-    if category == YACHT:
-        return yacht(dice)
-    elif category == ONES:
-        return category_number(dice, 1)
+    if category == ONES:
+        return sum_equals(dice, 1)
     elif category == TWOS:
-        return category_number(dice, 2)
+        return sum_equals(dice, 2)
     elif category == THREES:
-        return category_number(dice, 3)
+        return sum_equals(dice, 3)
     elif category == FOURS:
-        return category_number(dice, 4)
+        return sum_equals(dice, 4)
     elif category == FIVES:
-        return category_number(dice, 5)
+        return sum_equals(dice, 5)
     elif category == SIXES:
-        return category_number(dice, 6)
+        return sum_equals(dice, 6)
+    elif category == YACHT:
+        return yacht(dice)
     elif category == FULL_HOUSE:
-        if four_of_a_kind(dice) == 0 and \
-                little_straight(dice) == 0 and \
-                big_straight(dice) == 0 and \
-                yacht(dice) == 0 and \
-                not check_double_pair(dice):
-            return full_house(dice)
-        else:
-            return 0
+        return full_house(dice)
     elif category == CHOICE:
-        return choice(dice)
+        return sum(dice)
     elif category == FOUR_OF_A_KIND:
         return four_of_a_kind(dice)
     elif category == LITTLE_STRAIGHT:
@@ -48,43 +43,32 @@ def score(dice, category):
         return big_straight(dice)
 
 
-def check_double_pair(dice):
-    counter = dict((x, dice.count(x)) for x in set(dice))
-    pair_count = 0
-    for key in counter:
-        if counter[key] == 2:
-            pair_count += 1
-
-    return pair_count == 2
-
-
 def yacht(dice):
-    if dice.count(dice[0]) == len(dice):
+    if len(set(dice)) == 1:
         return 50
-    else:
-        return 0
+    return 0
 
 
-def category_number(dice, number):
+def sum_equals(dice, number):
     occurrences = dice.count(number)
-    if occurrences == 0:
-        return 0
-    else:
+    if 0 < occurrences:
         return number * occurrences
-
-
-def choice(dice):
-    return sum(dice)
+    return 0
 
 
 def full_house(dice):
-    return sum(dice)
+    counter = Counter(dice)
+    values = list(counter.values())
+    values.sort()
+    if values == [2, 3]:
+        return sum(dice)
+    return 0
 
 
 def four_of_a_kind(dice):
     counter = dict((x, dice.count(x)) for x in set(dice))
     for key in counter:
-        if counter[key] == 4 or counter[key] == 5:
+        if 4 <= counter[key]:
             return key * 4
     return 0
 
@@ -92,12 +76,10 @@ def four_of_a_kind(dice):
 def little_straight(dice):
     if sum(dice) == sum([1, 2, 3, 4, 5]):
         return 30
-    else:
-        return 0
+    return 0
 
 
 def big_straight(dice):
     if sum(dice) == sum([2, 3, 4, 5, 6]):
         return 30
-    else:
-        return 0
+    return 0
