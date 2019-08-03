55dab97fcba912e185b29e9c6804181755a9329e
Daniel Romero <romerodaniel@protonmail.com>
2019-08-03 10:55:28 +0200

    csharp hamming

---
+++ csharp/hamming/Hamming.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException();
+        }
+
+        int distance = 0;
+
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            if (firstStrand[i] != secondStrand[i])
+            {
+                distance++;
+            }
+        }
+
+        return distance;
+
+        // return firstStrand.Where((firstStrandElement, idx) => firstStrandElement != secondStrand[idx]).Count();
+    }
+}
