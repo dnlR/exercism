d3d363a5de36d5337ad07cfdeb5cdcc589cab349
ddnlr <romerodaniel@protonmail.com>
2020-02-01 11:52:55 +0100

    csharp pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,18 @@
+﻿public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        input = input.ToUpper();
+
+        foreach (var letter in alphabet)
+        {
+            if (!input.Contains(letter))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
