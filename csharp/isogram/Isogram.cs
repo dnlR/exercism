6a7b541105f875718355be284c73e49dc085f89c
ddnlr <romerodaniel@protonmail.com>
2020-02-02 13:42:51 +0100

    csharp isogram

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var hs = new HashSet<char>();
+        return word
+            .Where(char.IsLetter)
+            .Select(char.ToLowerInvariant)
+            .All(hs.Add);
+    }
+}
