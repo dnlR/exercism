dfa5fd0a1bd246aae4ac0ed056c8964e4167acbf
ddnlr <romerodaniel@protonmail.com>
2020-02-06 11:43:25 +0100

    csharp acronym

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        char[] splitters = {' ', '_', '-'};
+        var phraseWords = phrase.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+
+        return phraseWords
+            .Aggregate(string.Empty, (current, word) => current + char.ToUpperInvariant(word[0]));
+    }
+}
